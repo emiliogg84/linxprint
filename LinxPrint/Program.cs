42a4f5aecc401a2a86b7f6afe9fd7a4f82f8e3fe
Emilio García <emiliogg84@live.com>
2015-07-20 11:00:23 -0700

    Adding a Visual Studio solution and a Windows Form project

---
+++ LinxPrint/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+    See LICENSE in the project root for license information.
+*/
+
+namespace LinxPrint
+{
+    using System;
+    using System.Windows.Forms;
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+        }
+    }
+}
